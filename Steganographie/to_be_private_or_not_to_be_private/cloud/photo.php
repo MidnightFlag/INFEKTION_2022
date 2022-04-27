13ddf18fd59d359decddc04b4deced3fa1413b43
Spownark <97993747+Spownark@users.noreply.github.com>
2022-04-27 14:17:32 +0200

    Add files via upload

---
+++ Steganographie/to_be_private_or_not_to_be_private/cloud/photo.php
@@ -0,0 +1,10 @@
+<section>
+          <table>
+            <tr>
+              <td><img width="453px" height="253px" src="categorie\photo\forest.jpg" alt="forest.jpg"></td>
+              <td><img width="453px" height="253px" src="categorie\photo\cherry-blossoms.jpg" alt="cherry-blossoms.jpg"></td>
+              <td><img width="453px" height="253px" src="categorie\photo\iceland.jpg" alt="iceland.jpg"></td>
+              <td><img width="453px" height="253px" src="categorie\photo\sunset.jpg" alt="sunset.jpg"></td>
+            </tr>
+          </table>
+</section>
